e437f49b80da19cf6ee2d9f82f9cff7d9ecfad9b
Caroline Bieuvelet <caroline.bieuvelet@gmail.com>
2017-12-01 22:34:39 +0100

    display none btn show

--- add-remove_div/app.js
+++ add-remove_div/app.js
@@ -1,4 +1,5 @@
 function fillAddBox(nbr) {
+  document.getElementById('btnShow'+nbr).style.display = "none";
   var rq = new XMLHttpRequest();
   rq.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -8,7 +9,9 @@ function fillAddBox(nbr) {
   rq.open("GET", nbr+".php", true);
   rq.send();
 }
+
 function emptyAddBox(nbr){
+  document.getElementById('btnShow'+nbr).style.display = "block";
   var addBox = document.getElementById('addBox'+nbr);
   var view = document.getElementById('view'+nbr);
   addBox.removeChild(view);
