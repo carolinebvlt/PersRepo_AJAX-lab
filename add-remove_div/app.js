04fd3244c517090787f92dfef97b16c0c16cf699
Caroline Bieuvelet <caroline.bieuvelet@gmail.com>
2017-12-01 22:03:13 +0100

    with nbr

--- add-remove_div/app.js
+++ add-remove_div/app.js
@@ -1,15 +1,15 @@
-function fillAddBox() {
+function fillAddBox(nbr) {
   var rq = new XMLHttpRequest();
   rq.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-     document.getElementById("addBox").innerHTML = this.responseText;
+     document.getElementById("addBox"+nbr).innerHTML = this.responseText;
     }
   };
   rq.open("GET", "view.php", true);
   rq.send();
 }
-function emptyAddBox(){
-  var addBox = document.getElementById('addBox');
+function emptyAddBox(nbr){
+  var addBox = document.getElementById('addBox'+nbr);
   var view = document.getElementById('view');
   addBox.removeChild(view);
 }
--- add-remove_div/view.php
+++ add-remove_div/view.php
@@ -1,6 +1,6 @@
 <?php $name = 'Caroline'; ?>
 <div id="view">
-  <input type="button" value="close" onclick="emptyAddBox()">
+  <input type="button" value="close" onclick="emptyAddBox(1)">
   <h1>TEST</h1>
   <p>
     Bonjour <strong><?php echo $name ?></strong> !
