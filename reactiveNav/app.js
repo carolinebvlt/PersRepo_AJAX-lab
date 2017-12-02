e3cc9157d26cf44292c5b72f571d4cb6080850ee
Caroline Bieuvelet <caroline.bieuvelet@gmail.com>
2017-12-02 16:04:58 +0100

    highlight li ok mais bug avec css :hover

--- reactiveNav/app.js
+++ reactiveNav/app.js
@@ -2,3 +2,11 @@ function viewOfS(nbr){
   var s4 = document.getElementById('s'+nbr);
   s4.scrollIntoView({behavior: "smooth", block: "start"});
 }
+function highlightLi(nbr){
+  var li = document.getElementById('ch'+nbr);
+  li.style.fontWeight = "bold";
+}
+function normalLi(nbr){
+  var li = document.getElementById('ch'+nbr);
+  li.style.fontWeight = "normal";
+}
